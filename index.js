b332f3cbe0b020f1c69dfd51bb0d5338ee797629
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-04-29 10:36:15 +0900

    정적 파일 접근을 src에서 허용하도록 개선

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.listen(8080,function(){
     console.log('listening on 8080');
 });
 
+app.use(express.static('src'));
+
 // 여기에서 URL을 등록
 
 app.get('/', function(req, res){
@@ -18,3 +20,8 @@ app.get('/bootstrap', function(req, res){
 app.get('/jquery', function(req, res){
     res.sendFile(__dirname + '/src/jquery/jquery.html');
 });
+
+app.get('/react', function(req, res){
+    res.sendFile(__dirname + '/src/react/react.html');
+});
+
---
+++ src/react/like_button.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// ... 복사했던 스타터 코드 ...
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
